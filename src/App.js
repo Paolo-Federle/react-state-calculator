4b11bcb66ce8ff206f09aadb111314c92123063e
Paolo Federle <federlepaolo95@gmail.com>
2022-09-21 18:11:46 +0200

    Core exercise done

--- src/App.js
+++ src/App.js
@@ -1,56 +1,98 @@
 import "./App.css"
+import { useState } from 'react'
 
 function App() { 
+  const [LeftNumber, setLeftNumber] = useState(0)
+  // const operationSimbol = useState('+')
+  const [operationSimbol, setOperationSimbol] = useState('+')
+  const [rightNumber, setRightNumber] = useState(0)
+  // const resultNumber = getResult()
+  const [resultNumber, setresultNumber] = useState(0)
 
+  const getLeftNumber = (e) => {
+    setLeftNumber(parseInt(LeftNumber+e.target.innerText, 10))
+  }
+
+  const getRightNumber = (e) => {
+    setRightNumber(parseInt(rightNumber+e.target.innerText, 10))
+  }
+
+  const getOperationSimbol = (e) => {
+    setOperationSimbol(e.target.innerText)
+  }
+  
+
+  function getResult(){
+    if (operationSimbol === "+") {
+      return setresultNumber(LeftNumber+rightNumber)
+    } else if (operationSimbol === "-") {
+      return setresultNumber(LeftNumber-rightNumber)
+    } else if (operationSimbol === "*") {
+      return setresultNumber(LeftNumber*rightNumber)
+    } else if (operationSimbol === "÷") {
+      return setresultNumber(LeftNumber/rightNumber)
+    }
+  }
+ 
+  
+  // const [LeftNumber, setLeftNumber] = useState(0)
+  // const [operationSimbol, setoperationSimbol] = useState('+')
+  // const [rightNumber, setrightNumber] = useState(0)
+  // const [resultNumber, setresultNumber] = useState(0)
+  // const [storeValue, setStoreValue] = useState(0)
+  // function setLeftNumber(inputNumber) {
+  //   return LeftNumber = inputNumber
+  // }
+  // const countStateArrayle = useState(0)
   return (
     <div className="calculator">
         <div className="panel">
-          <p>0</p>
+          <p>{LeftNumber}</p>
           <div className="numbers">
-            <button>1</button>
-            <button>2</button>
-            <button>3</button>
-            <button>4</button>
-            <button>5</button>
-            <button>6</button>
-            <button>7</button>
-            <button>8</button>
-            <button>9</button>
-            <button>0</button>
-            <button>Clear</button>
+            <button onClick={getLeftNumber}>1</button>
+            <button onClick={getLeftNumber}>2</button>
+            <button onClick={getLeftNumber}>3</button>
+            <button onClick={getLeftNumber}>4</button>
+            <button onClick={getLeftNumber}>5</button>
+            <button onClick={getLeftNumber}>6</button>
+            <button onClick={getLeftNumber}>7</button>
+            <button onClick={getLeftNumber}>8</button>
+            <button onClick={getLeftNumber}>9</button>
+            <button onClick={getLeftNumber}>0</button>
+            <button onClick={() => setLeftNumber(0)}>Clear</button>
           </div>
         </div>
         
         <div className="panel">
-          <p>+</p>
+          <p>{operationSimbol}</p>
           <div className="numbers">
-            <button>+</button>
-            <button>-</button>
-            <button>*</button>
-            <button>÷</button>
+            <button onClick={getOperationSimbol}>+</button>
+            <button onClick={getOperationSimbol}>-</button>
+            <button onClick={getOperationSimbol}>*</button>
+            <button onClick={getOperationSimbol}>÷</button>
           </div>
         </div>
 
         <div className="panel">
-          <p>0</p>
+          <p>{rightNumber}</p>
           <div className="numbers">
-            <button>1</button>
-            <button>2</button>
-            <button>3</button>
-            <button>4</button>
-            <button>5</button>
-            <button>6</button>
-            <button>7</button>
-            <button>8</button>
-            <button>9</button>
-            <button>0</button>
-            <button>Clear</button>
+            <button onClick={getRightNumber}>1</button>
+            <button onClick={getRightNumber}>2</button>
+            <button onClick={getRightNumber}>3</button>
+            <button onClick={getRightNumber}>4</button>
+            <button onClick={getRightNumber}>5</button>
+            <button onClick={getRightNumber}>6</button>
+            <button onClick={getRightNumber}>7</button>
+            <button onClick={getRightNumber}>8</button>
+            <button onClick={getRightNumber}>9</button>
+            <button onClick={getRightNumber}>0</button>
+            <button onClick={() => setRightNumber(0)}>Clear</button>
           </div>
         </div>
         <div className="panel answer">
-          <p>0</p>
+          <p>{resultNumber}</p>
           <div>
-            <button>=</button>
+            <button onClick={getResult}>=</button>
           </div>
         </div>
     </div>
